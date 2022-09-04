4a5556af55193a1e07a00e7e8c650ddda1271726
Troy Marchesseault <tmarchess@gmail.com>
2022-09-04 14:33:47 -0400

    Add access to Tribute POST endpoint

--- src/components/pages/Tributes.jsx
+++ src/components/pages/Tributes.jsx
@@ -1,9 +1,46 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
 import TributeOverview from "../TributeOverview";
 
 const Tributes = () => {
     const [tributes, setTributes] = useState([]);
     const [name, setName] = useState("");
+    useEffect(() => {
+        axios({
+            method: "get",
+            url: "http://localhost:3333/api/tribute",
+        })
+            .then((response) => {
+                setTributes(response.data.tributes);
+            })
+            .catch((response) => {
+                console.log("Error! Could not get tributes!");
+                console.log(response);
+            });
+    }, []);
+
+    const handleAdd = () => {
+        if (name) {
+            axios({
+                method: "post",
+                url: "http://localhost:3333/api/tribute",
+                data: {
+                    name,
+                },
+            })
+                .then((response) => {
+                    console.log("Success! Added tribute");
+                    console.log(response.data);
+                    setTributes([...tributes, name]);
+                    setName("");
+                })
+                .catch((response) => {
+                    console.log("Error! Could not add tribute!");
+                    console.log(response);
+                });
+        }
+    };
 
     return (
         <>
@@ -12,16 +49,7 @@ const Tributes = () => {
                 <input value={name} onChange={(e) => setName(e.target.value)} />
             </div>
             <div>
-                <button
-                    onClick={() => {
-                        if (name) {
-                            setTributes([...tributes, name]);
-                            setName("");
-                        }
-                    }}
-                >
-                    +
-                </button>
+                <button onClick={handleAdd}>+</button>
                 Add
             </div>
             <div>
